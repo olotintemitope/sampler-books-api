96512cae7e6c45b9987d0cc8980c6bb8caa57177
Temitope Olotin <olotintemitope@gmail.com>
2021-02-16 21:41:00 +0100

    add auth api middleware

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,8 @@ Route::group([
     'namespace' => 'Api',
 ], function () {
     Route::get('/user', 'UserController@getAll')->name('api.user_all');
-    Route::post('/user', 'UserController@create')->name('api.user_create');
-    Route::put('/user/{id}/update', 'UserController@update')->name('api.user_update');
-
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('/user', 'UserController@create')->name('api.user_create');
+        Route::put('/user/{id}/update', 'UserController@update')->name('api.user_update');
+    });
 });
