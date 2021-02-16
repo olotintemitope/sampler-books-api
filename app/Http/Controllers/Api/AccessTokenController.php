09fb0440135826862e76cfc702dcc7fa862afd5f
Temitope Olotin <olotintemitope@gmail.com>
2021-02-16 22:59:31 +0100

    assert that user can login

---
+++ app/Http/Controllers/Api/AccessTokenController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+/**
+ * Class AccessTokenController
+ * @package App\Http\Controllers\Api
+ */
+class AccessTokenController extends BaseController
+{
+    //
+}
--- tests/Unit/AuthenticationTest.php
+++ tests/Unit/AuthenticationTest.php
@@ -4,7 +4,32 @@
 namespace Tests\Unit;
 
 
-class AuthenticationTest
-{
+use Tests\TestCase;
 
+/**
+ * Class AuthenticationTest
+ * @package Tests\Unit
+ */
+class AuthenticationTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testThatUserCanLogin(): void
+    {
+        $res = $this->json(
+            'POST',
+            route('api.user_create'),
+            []);
+    }
+
+    public function tearDown(): void
+    {
+        try {
+            parent::tearDown();
+        } catch (\Throwable $e) {
+        }
+    }
 }
