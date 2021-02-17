07ed5b9e291d544b4b88a004947fad6c45a31796
Temitope Olotin <olotintemitope@gmail.com>
2021-02-17 20:02:41 +0100

    test that book route is available

--- tests/Unit/BookControllerTest.php
+++ tests/Unit/BookControllerTest.php
@@ -1,11 +1,25 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
+use Tests\AuthorizationTrait;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class BookControllerTest extends TestCase
 {
+    use RefreshDatabase;
+    use AuthorizationTrait;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('passport:install');
+    }
+
+    public function testSeeRoute(): void
+    {
+        $res = $this->json('GET', route('api.book_all'));
+    }
 }
